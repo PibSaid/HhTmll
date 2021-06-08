f36b8f8b113f1e869ce4f03e1bc868f5d3634936
ZhenyaH <Zhenyaxomchenko00@mail.ru>
2021-06-09 00:44:17 +0300

    Com7

---
+++ Ком7/style/script.js
@@ -0,0 +1,25 @@
+function books(){
+
+document.getElementById(&quot;book&quot;).style.visibility=&#39;visible&#39;;
+document.getElementById(&quot;movie&quot;).style.visibility=&#39;hidden&#39;;
+document.getElementById(&quot;musik&quot;).style.visibility=&#39;hidden&#39;;
+document.getElementById(&quot;game&quot;).style.visibility=&#39;hidden&#39;;
+}
+function movies(){
+document.getElementById(&quot;book&quot;).style.visibility=&#39;hidden&#39;;
+document.getElementById(&quot;movie&quot;).style.visibility=&#39;visible&#39;;
+document.getElementById(&quot;musik&quot;).style.visibility=&#39;hidden&#39;;
+document.getElementById(&quot;game&quot;).style.visibility=&#39;hidden&#39;;
+}
+function musiks(){
+document.getElementById(&quot;book&quot;).style.visibility=&#39;hidden&#39;;
+document.getElementById(&quot;movie&quot;).style.visibility=&#39;hidden&#39;;
+document.getElementById(&quot;musik&quot;).style.visibility=&#39;visible&#39;;
+document.getElementById(&quot;game&quot;).style.visibility=&#39;hidden&#39;;
+}
+function games(){
+document.getElementById(&quot;book&quot;).style.visibility=&#39;hidden&#39;;
+document.getElementById(&quot;movie&quot;).style.visibility=&#39;hidden&#39;;
+document.getElementById(&quot;musik&quot;).style.visibility=&#39;hidden&#39;;
+document.getElementById(&quot;game&quot;).style.visibility=&#39;visible&#39;;
+}
